e4ae5755b8783844d6e06857d0dce46b8b68f200
Jasani <nikhil.jasani@capgemini.com>
2017-02-23 15:29:14 +0530

    First commit

---
+++ GitDemo/src/org/cg/irp/main/GitDemo.java
@@ -0,0 +1,14 @@
+package org.cg.irp.main;
+
+import org.cg.irp.utility.Printer;
+
+/**
+ * @author njasani
+ *
+ */
+public class GitDemo {
+
+	public static void main(String[] args) {
+		Printer.printOnConsole("This is for Git demo");
+	}
+}
