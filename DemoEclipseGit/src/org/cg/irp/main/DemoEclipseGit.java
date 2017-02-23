57b30cd9bff09180a1cbe1044609c6bfe34c4137
njasani <nikhil.jasani@capgemini.com>
2017-02-23 15:48:41 +0530

    First Commit from Eclipse

---
+++ DemoEclipseGit/src/org/cg/irp/main/DemoEclipseGit.java
@@ -0,0 +1,15 @@
+package org.cg.irp.main;
+
+/**
+ * @author njasani
+ *
+ */
+public class DemoEclipseGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git demo from Eclipse");
+	}
+}
